a6ae5f0898b224f6b6a505aa7302b21ea8ba64aa
nikolaevilya82 <n.ilya@yandex.ru>
2024-08-10 13:07:51 +0300

    Parsing of product links.

--- parser/parser.py
+++ parser/parser.py
@@ -5,20 +5,23 @@ from bs4 import BeautifulSoup
 def parsing_product(product_link: str):
     product_response = requests.get(product_link).text
     product_object = BeautifulSoup(product_response, 'lxml')
-    block_body = product_object.find("body", style="padding-right: 0px;")
+    block_body = product_object.find("body", style=None)
     block_tool = block_body.find("div", id="tool")
     block_wrapper = block_tool.find("div", id="calorizator_wrapper")
     block_product = block_wrapper.find("div", id="calorizator_products")
-    block_wrapper2 = block_product.find("div", id="product_wrapper")
-    block_links = block_wrapper2.find_all("a")
-    list_of_links = []
+    block_wrapper2 = block_product.find("div", id="products_wrapper")
+    block_product2 = block_wrapper2.find_all("div", class_="product")
+    list_of_href = []
 
-    for i_block2 in block_links:
-        links2 = i_block2.find_all("a")
-        for i_link2 in links2:
-            href2 = i_link2.get('href')
-            list_of_links.append(product_link + href2)
-    return list_of_links
+    for i in block_product2:
+        block_links = i.find("a")
+        list_of_href.append(block_links.get("href"))
+
+    list_of_full_links = []
+    for i_href in list_of_href:
+        list_of_full_links.append(product_link + i_href)
+    return list_of_full_links
+
 
 link = "https://supercalorizator.ru/"
 response = requests.get(link).text
@@ -47,7 +50,8 @@ print(list_of_links)
 
 list_of_url = []
 for i_url in list_of_links:
+    for j_url in parsing_product(i_url):
+        list_of_url.append(j_url)
 
-
-
+print(list_of_url)
 
