90c4f0813f50123c5b261dc8166afa6d07c3f5ff
nikolaevilya82 <n.ilya@yandex.ru>
2024-09-01 18:03:39 +0300

    Created  survey.

--- handlers/custom_handlers/survey.py
+++ handlers/custom_handlers/survey.py
@@ -7,12 +7,17 @@ load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
+survey_parameters = tuple()
 
 
 def take_survey(chat_id):
     bot.send_message(chat_id,"Выберите Ваш пол.",
                      reply_markup=utils.is_gender())
 
+
+@bot.message_handler(func=lambda message: message.text == "man")
+def add_gender():
+    return ("man",)
 
 
 if __name__ == "__main__":
--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 import telebot
+from telebot.types import ReplyKeyboardRemove
 from keyboards.reply import utils
+from handlers.custom_handlers import survey
 
 load_dotenv()
 
@@ -11,10 +13,20 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.send_message(message.from_user.id, f"Здравствуйте{"""name"""}"
-                              f"!Это бот, который считает калории съеденных Вами продуктов за день. "
-                              f"Для того, что бы рассчитать Вашу суточную норму калорий пройдите опрос",
+    bot.send_message(message.from_user.id,
+                f"Здравствуйте{"""name"""}"
+                     f"!Это бот, который считает калории съеденных Вами продуктов за день. "
+                     f"Для того, что бы рассчитать Вашу суточную норму калорий пройдите опрос",
                      reply_markup=utils.gen_markup())
+
+
+@bot.message_handler(func=lambda message: message.text == "Пройти опрос.")
+def get_survey(message):
+    survey.take_survey(message.chat.id)
+    bot.send_message(
+        message.from_user.id,
+         f"Ваша суточная норма калорий {"""calories"""}",
+        reply_markup=ReplyKeyboardRemove())
 
 
 if __name__ == '__main__':
