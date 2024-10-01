27145f69ffd7f1675f512c8b09dc586a8a3fc0a3
nikolaevilya82 <n.ilya@yandex.ru>
2024-10-01 19:48:40 +0300

    Created first table.

--- database/common/models.py
+++ database/common/models.py
@@ -5,22 +5,23 @@ import peewee as pw
 db = pw.SqliteDatabase('users_tgb.db')
 
 
-class ModelBase(pw.Model):
-    created_at = pw.DateField(default=datetime.now())
+class BaseModel(pw.Model):
 
     class Meta:
         database = db
 
 
-class User(ModelBase):
-    tg_id = pw.IntegerField
-    user_name = pw.TextField
-    user_gender = pw.TextField
-    user_age = pw.IntegerField
-    user_weight = pw.IntegerField
-    user_height = pw.IntegerField
-    daily_norm = pw.IntegerField
+class User(BaseModel):
+    class Meta:
+        db_table = 'Users'
+    tg_id = pw.IntegerField()
+    user_name = pw.CharField(max_length=150)
+    user_gender = pw.CharField(max_length=150)
+    user_age = pw.IntegerField()
+    user_weight = pw.IntegerField()
+    user_height = pw.IntegerField()
+    daily_norm = pw.IntegerField()
 
 
-if __name__ == '__main__':
-    db.create_tables([User])
+db.create_tables([User])
+User(tg_id=1, user_name='name', user_gender='gender', user_age=10, user_weight=20, user_height=30, daily_norm=50).save()
--- database/core.py
+++ database/core.py
@@ -1,14 +1,11 @@
-from database.common import models
-from database.utils.CRUD import CRUDInterface
-from handlers.default_handlers.survey import survey_parameters
+from database.common.models import User
+#from handlers.default_handlers.survey import survey_parameters
 
 
-models.db.create_tables([models.User])
-
-user = models.User(survey_parameters['user_id'], survey_parameters['name'], survey_parameters['gender'],
-                   survey_parameters['age'], survey_parameters['weight'], survey_parameters['height'],
-                   survey_parameters['daily_norm'])
-user.save()
+# user = User(survey_parameters['user_id'], survey_parameters['name'], survey_parameters['gender'],
+#             survey_parameters['age'], survey_parameters['weight'], survey_parameters['height'],
+#             survey_parameters['daily_norm'])
+User(1, 'name', 'gender', 10, 20, 30, 50).save()
 
 
 
